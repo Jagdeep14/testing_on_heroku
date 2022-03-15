249908f8f27334e7180d9128776e26e04e7db1a1
Jagdeep14 <sidhu.deepi87@gmail.com>
2022-03-14 22:41:17 -0700

    added files

---
+++ init.R
@@ -0,0 +1,54 @@
+# R script to run author supplied code, typically used to install additional R packages
+# contains placeholders which are inserted by the compile script
+# NOTE: this script is executed in the chroot context; check paths!
+
+r <- getOption('repos')
+r['CRAN'] <- 'http://cloud.r-project.org'
+options(repos=r)
+
+# ======================================================================
+
+#we used these libraries, so will need to install all packages below...
+# library(dash)
+# library(dashHtmlComponents)
+# library(dashCoreComponents)
+# library(dashBootstrapComponents)
+# library(devtools)
+# library(ggplot2)
+# library(plotly)
+# library(tidyverse)
+# library(readxl)
+# library(hexbin)
+# library(ggthemes)
+# library(repr)
+# library(lubridate)
+# library(stringr)
+# library(reshape2)
+# library(gridExtra)
+# library(OpenImageR)
+
+
+# packages go here
+install.packages('remotes')
+
+install.packages('dash')
+install.packages('dashHtmlComponents')
+install.packages('dashCoreComponents')
+install.packages('dashBootstrapComponents')
+install.packages('devtools')
+install.packages('ggplot2')
+install.packages('plotly')
+install.packages('tidyverse')
+install.packages('readxl')
+install.packages('hexbin')
+install.packages('ggthemes')
+install.packages('repr')
+install.packages('lubridate')
+install.packages('stringr')
+install.packages('reshape2')
+install.packages('gridExtra')
+install.packages('OpenImageR')
+
+remotes::install_github('plotly/dashR', upgrade=TRUE)
+remotes::install_github('facultyai/dash-bootstrap-components@r-release')
+
