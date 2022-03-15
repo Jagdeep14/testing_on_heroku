1a8fbc2bfc05c29ac73e322d0ea4b45402432cb4
Jagdeep Brar <87039377+Jagdeep14@users.noreply.github.com>
2022-03-14 23:27:27 -0700

    fixed init

--- init.R
+++ init.R
@@ -40,9 +40,9 @@ install.packages('ggplot2')
 install.packages('plotly')
 install.packages('tidyverse')
 install.packages('reshape2')
-install.packages('OpenImageR')
+#install.packages('OpenImageR')
 
 remotes::install_github('plotly/dashR', upgrade=TRUE)
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
-remotes::install_github('mlampros/OpenImageR')
+devtools::install_github('mlampros/OpenImageR')
 
